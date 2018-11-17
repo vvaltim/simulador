90e2b32cac57a49f5e8b51c2cb3b5ab362146767
Walter Júnior <vvaltim@gmail.com>
2018-11-17 16:15:37 -0200

    Melhorias

--- src/app/pages/manageClass/listClassCtrl.js
+++ src/app/pages/manageClass/listClassCtrl.js
@@ -5,21 +5,32 @@
         .controller('ListClassCtrl', ListClassCtrl);
 
     /** @ngInject */
-    function ListClassCtrl($scope, $state, $uibModal) {
-        $scope.classes = [{
-            id: "0",
-            name: "Teste 1",
-            period: "Primeiro Período",
-        }];
-        $scope.openExercise = function () {
-            $state.go('createClass');
+    function ListClassCtrl($scope, $state, $uibModal, $http) {
+        //atualizando a lista de turmas
+        updateListTeam();
+
+        // Criando uma turma
+        $scope.createClass = function (newClass) {
+            newClass.codTeacher = parseInt(localStorage.getItem("codTeacher"));
+            newClass.active = true;
+            console.log(newClass);
+
+            $http({
+                method: 'POST',
+                url: 'http://localhost:8080/team/create',
+                data: newClass
+            }).then(function success(response) {
+                updateListTeam()
+            }, function onError(response) {
+                console.log(response);
+            });
+
+            //TODO: Atualizar a lista de turmas
+            
         }
 
-        $scope.createClass = function (newClass) {
-            newClass.id = Math.floor(Math.random() * 100)
-            console.log(newClass);
-            $scope.classes.push(newClass);
-            //TODO: Chamar o serviço de persistência
+        $scope.openExercise = function () {
+            $state.go('createClass');
         }
 
         $scope.editClass = function (selectedItem) {
@@ -29,7 +40,7 @@
         $scope.deleteClass = function (selectedItem) {
             $uibModal.open({
                 animation: true,
-                template: '<div class="modal-content"><div class="modal-header bg-danger"><i class="ion-android-danger modal-icon"></i><span> Atenção</span></div><div class="modal-body text-center">Tem certeza que deseja excluir a  Turma ' + selectedItem.name + '?</div><div class="modal-footer"><button type="button" class="btn btn-danger" ng-click="$dismiss()">Excluir</button></div></div>',
+                template: '<div class="modal-content"><div class="modal-header bg-danger"><i class="ion-android-danger modal-icon"></i><span> Atenção</span></div><div class="modal-body text-center">Tem certeza que deseja excluir a  Turma ' + selectedItem.discipline + '?</div><div class="modal-footer"><button type="button" class="btn btn-danger" ng-click="$dismiss()">Excluir</button></div></div>',
                 size: 'md',
                 resolve: {
                     items: function () {
@@ -38,12 +49,23 @@
                     }
                 }
             });
-        }
+        };
 
         $scope.saveTeacher = function(codTeacher){
             localStorage.setItem("codTeacher", parseInt(codTeacher));
-        }
+        };
 
+        function updateListTeam(){
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8080/team/allTeam/' + localStorage.getItem("codTeacher")
+            }).then(function success(response) {
+                $scope.classes = response.data
+                console.log(response.data);
+            }, function onError(response) {
+                console.log(response.data);
+            });
+        }
     }
 
 })();
--- src/app/pages/postCase/postCaseCtrl.js
+++ src/app/pages/postCase/postCaseCtrl.js
@@ -5,11 +5,11 @@
         .controller('PostCaseCtrl', PostCaseCtrl);
 
     /** @ngInject */
-    function PostCaseCtrl($scope, $http) {
-        var codTeacher = localStorage.getItem("codTeacher");
+    function PostCaseCtrl($scope, $http, toastr) {
+        //pegando todas as turmas do usuário logado
         $http({
             method: 'GET',
-            url: 'http://localhost:8080/team/allTeam/' + codTeacher
+            url: 'http://localhost:8080/team/allTeam/' + localStorage.getItem("codTeacher")
         }).then(function success(response) {
             $scope.classes = response.data
             console.log(response.data);
@@ -17,6 +17,7 @@
             console.log(response.data);
         });
 
+        //salvando o caso com as turmas
         $scope.saveCase = function (classes) {
             var request = [];
             angular.forEach(classes, function (value, key) {
@@ -29,15 +30,49 @@
                     request.push(exercise);
                 }
             })
+            console.log(request);
             $http({
                 method: 'POST',
                 url: 'http://localhost:8080/exercise/create/',
                 data: request
             }).then(function success(response) {
-                $scope.classes = response.data
-                console.log(response.data);
+            
+                //feedback visual de sucesso
+                toastr.success('', 'Caso postado com sucesso', {
+                    "autoDismiss": false,
+                    "positionClass": "toast-bottom-right",
+                    "type": "success",
+                    "timeOut": "5000",
+                    "extendedTimeOut": "2000",
+                    "allowHtml": false,
+                    "closeButton": false,
+                    "tapToDismiss": true,
+                    "progressBar": false,
+                    "newestOnTop": true,
+                    "maxOpened": 0,
+                    "preventDuplicates": false,
+                    "preventOpenDuplicates": false
+                  });
+
             }, function onError(response) {
-                console.log(response.data);
+
+                //feedback visual de erro
+                toastr.error('', 'Não foi possível enviar o caso.', {
+                    "autoDismiss": false,
+                    "positionClass": "toast-bottom-right",
+                    "type": "error",
+                    "timeOut": "5000",
+                    "extendedTimeOut": "2000",
+                    "allowHtml": false,
+                    "closeButton": false,
+                    "tapToDismiss": true,
+                    "progressBar": false,
+                    "newestOnTop": true,
+                    "maxOpened": 0,
+                    "preventDuplicates": false,
+                    "preventOpenDuplicates": false
+                  })
+
             });
         }
     }
